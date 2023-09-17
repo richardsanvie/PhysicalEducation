c4ea513a0d392342866ab27441dc3c657eced4ec
Richard <richardsanvie@gmail.com>
2023-09-17 10:28:21 -0300

    Initial commit

---
+++ src/functions.js
@@ -0,0 +1,15 @@
+"use strict";
+// MultiplicaNumeros
+function calculaArea(base, altura) {
+    return base * altura;
+}
+// const calculaArea2=(base:number,altura:number):number=>base*altura;
+//Saudacao
+function saudacao(nome) {
+    return (`Olá ${nome}!`);
+}
+// console.log
+const CalculoArea1 = calculaArea(5, 4);
+const sauda = saudacao("Richard Sanvie");
+console.log(CalculoArea1);
+console.log(sauda);
---
+++ src/functions.ts
@@ -0,0 +1,20 @@
+// MultiplicaNumeros
+function calculaArea(base: number, altura: number): number {
+    return base * altura;
+}
+
+// const calculaArea2=(base:number,altura:number):number=>base*altura;
+
+//Saudacao
+
+function saudacao(nome: string): string {
+    return (`Olá ${nome}!`);
+}
+
+// console.log
+
+const CalculoArea1 = calculaArea(5, 4);
+const sauda = saudacao("Richard Sanvie");
+
+console.log(CalculoArea1);
+console.log(sauda);
